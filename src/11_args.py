2b7b08d70ff73b535c49b2b8e7fea3272a821bee
Wade-Coplen <wadecoplen@gmail.com>
2020-09-06 19:52:18 -0700

    11 incomplete

--- src/11_args.py
+++ src/11_args.py
@@ -5,7 +5,8 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
-
+def f1(x, y):
+    return x + y
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and returns the
@@ -13,6 +14,12 @@ print(f1(1, 2))
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+def f2(*sum):
+    result = 0
+    for i in sum:
+        result += i
+    return result
+print(f2(2, 4, 6))
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -20,10 +27,11 @@ print(f2(1, 4, -12))            # Should print -7
 print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 
 a = [7, 6, 5, 4]
+##We need to access each index in the list. how do acces one index in a list?
 
 # How do you have to modify the f2 call below to make this work?
-print(f2(a))    # Should print 22
-
+print(f2(a++))    # Should print 22
+    
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments.
